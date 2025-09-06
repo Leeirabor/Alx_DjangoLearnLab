74963b369bea183096dabcaa38cd9f5d8204447a
leeirabor <elishairabor22@gmail.com>
2025-09-06 10:33:13 +0100

    Add querysampe.py to relationship_app models

---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,16 @@
+from relationship_app.models import Author, Book, Library, Librarian
+
+# Query all books by a specific author
+books_by_orwell = Book.objects.filter(author__name="George Orwell")
+
+# List all books in a library
+library = Library.objects.get(name="Central Library")
+books_in_library = library.books.all()
+
+# Retrieve the librarian for a library
+librarian = library.librarian
+
+# Print results (optional for testing)
+print("Books by George Orwell:", [book.title for book in books_by_orwell])
+print("Books in Central Library:", [book.title for book in books_in_library])
+print("Librarian of Central Library:", librarian.name)
