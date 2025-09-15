b94c296773c08c232cdede521fe61c823a3a616e
Irabor <eirabor@xceneresearch.com>
2025-09-15 20:50:01 +0100

    adding advanced_fetures_and_security

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -51,7 +51,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+
+AUTH_USER_MODEL = "bookshelf.CustomUser"
+
+
 AUTH_USER_MODEL = "relationship_app.CustomUser"
+
 
 
 ROOT_URLCONF = 'LibraryProject.urls'
@@ -130,3 +135,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "bookshelf.CustomUser"
