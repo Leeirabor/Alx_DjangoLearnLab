d3a818b52e73ebc79f07920c3cb5ecc308669e4c
Irabor <eirabor@xceneresearch.com>
2025-09-15 23:33:12 +0100

    adding advanced_fetures_and_security

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -4,6 +4,18 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework import viewsets
+from .models import Book
+from .serializers import BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    A ViewSet for CRUD operations on Book model.
+    Supports: list, retrieve, create, update, delete.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
