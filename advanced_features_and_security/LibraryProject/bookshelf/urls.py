9684a0c100768d82da48b28ed4ca12abe76b1ca5
Irabor <eirabor@xceneresearch.com>
2025-09-15 21:28:02 +0100

    adding advanced_fetures_and_security

--- advanced_features_and_security/LibraryProject/bookshelf/urls.py
+++ advanced_features_and_security/LibraryProject/bookshelf/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("books/", views.book_list, name="book_list"),
     path("books/", views.view_books, name="view_books"),
     path("books/add/", views.add_book, name="add_book"),
     path("books/<int:pk>/edit/", views.edit_book, name="edit_book"),
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Book
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import permission_required
+from .models import Book
 
 # View books (requires can_view)
 @permission_required("bookshelf.can_view", raise_exception=True)
@@ -40,3 +43,40 @@ def delete_book(request, pk):
     book = get_object_or_404(Book, pk=pk)
     book.delete()
     return redirect("view_books")
+
+
+# Book list view (requires can_view)
+@permission_required("bookshelf.can_view", raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, "bookshelf/book_list.html", {"books": books})
+
+# Create book (requires can_create)
+@permission_required("bookshelf.can_create", raise_exception=True)
+def add_book(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        author = request.POST.get("author")
+        publication_year = request.POST.get("publication_year")
+        Book.objects.create(title=title, author=author, publication_year=publication_year)
+        return redirect("book_list")
+    return render(request, "bookshelf/add_book.html")
+
+# Edit book (requires can_edit)
+@permission_required("bookshelf.can_edit", raise_exception=True)
+def edit_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == "POST":
+        book.title = request.POST.get("title")
+        book.author = request.POST.get("author")
+        book.publication_year = request.POST.get("publication_year")
+        book.save()
+        return redirect("book_list")
+    return render(request, "bookshelf/edit_book.html", {"book": book})
+
+# Delete book (requires can_delete)
+@permission_required("bookshelf.can_delete", raise_exception=True)
+def delete_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    book.delete()
+    return redirect("book_list")
