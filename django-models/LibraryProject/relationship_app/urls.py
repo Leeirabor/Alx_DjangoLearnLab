2919ce66996ec748107d5cfd987538603534e1c9
Irabor <eirabor@xceneresearch.com>
2025-09-15 15:00:57 +0100

    adding views.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path("books/", views.list_books, name="list_books"),  # function-based view
     path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),  # class-based view
+    path("books/", views.list_books, name="list_books"),
+    path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
 ]
+
