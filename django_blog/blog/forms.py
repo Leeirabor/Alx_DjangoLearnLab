97a9bc4827d09040ed4de50aef09381b48dd9baf
Irabor <eirabor@xceneresearch.com>
2025-10-02 16:35:08 +0100

    updated blog app with post

---
+++ django_blog/blog/forms.py
@@ -0,0 +1,34 @@
+# blog/forms.py
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Post
+
+
+class PostForm(forms.ModelForm):
+    """
+    Form for creating/updating Post instances.
+    The author is set in the view (request.user), not by the form.
+    """
+    class Meta:
+        model = Post
+        fields = ['title', 'content']  # author & published_date are set automatically
+        widgets = {
+            'title': forms.TextInput(attrs={'placeholder': 'Post title'}),
+            'content': forms.Textarea(attrs={'rows': 10, 'placeholder': 'Write your post...'}),
+        }
+
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True, label="Email address")
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ("first_name", "last_name", "email")
+
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -4,9 +4,76 @@ from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+from .forms import PostForm
 
 from .forms import RegisterForm, ProfileForm
 from .models import Post
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/post_list.html"
+    context_object_name = "posts"
+    paginate_by = 10
+    ordering = ["-published_date"]
+
+
+# Public detail view
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/post_detail.html"
+    context_object_name = "post"
+
+
+# Authenticated users can create posts
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = "blog/post_form.html"
+    # where to go after successful creation
+    success_url = reverse_lazy("post-list")
+    login_url = "login"
+
+    def form_valid(self, form):
+        # set the logged-in user as author
+        form.instance.author = self.request.user
+        messages.success(self.request, "Post created successfully.")
+        return super().form_valid(form)
+
+
+# Only the author can update
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = "blog/post_form.html"
+    success_url = reverse_lazy("post-list")
+    login_url = "login"
+
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
+
+    def handle_no_permission(self):
+        messages.error(self.request, "You are not allowed to edit this post.")
+        return super().handle_no_permission()
+
+
+# Only the author can delete
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = "blog/post_confirm_delete.html"
+    success_url = reverse_lazy("post-list")
+    login_url = "login"
+
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
+
+    def handle_no_permission(self):
+        messages.error(self.request, "You are not allowed to delete this post.")
+        return super().handle_no_permission()
 
 def home(request):
     # simple homepage listing recent posts
