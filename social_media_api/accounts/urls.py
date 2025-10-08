ac99a23551b3e2c9fe3c63a3e6a02507c97d5b31
Irabor <eirabor@xceneresearch.com>
2025-10-09 00:17:16 +0100

    --adjust

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path("login/", CustomObtainAuthToken.as_view(), name="login"),
     path("profile/", ProfileView.as_view(), name="profile"),
     path("follow/<str:username>/", FollowToggleView.as_view(), name="follow-toggle"),
+    
+    path('follow/<int:user_id>/', views.follow_user, name='follow_user'),
+    path('unfollow/<int:user_id>/', views.unfollow_user, name='unfollow_user'),
+
 ]
